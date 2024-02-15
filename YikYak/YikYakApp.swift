b3ef4813475992fd7888219a3d920aadce805ed8
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2024-02-15 16:42:36 -0500

    Initial Commit

---
+++ YikYak/YikYakApp.swift
@@ -0,0 +1,17 @@
+//
+//  YikYakApp.swift
+//  YikYak
+//
+//  Created by AlecNipp on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct YikYakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
